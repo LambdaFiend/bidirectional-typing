19820577029bf5a94dfce82c369dba859094f9fc
Gabriel Carlos <42923229+LambdaFiend@users.noreply.github.com>
2026-03-12 01:02:36 +0000

    Quick fix

--- src/Typing.hs
+++ src/Typing.hs
@@ -9,7 +9,7 @@ synth' t = synth [] t
 synth :: TypeContext -> TermNode -> Type
 synth ctx t = let tm = getTm t in
   case tm of
-    TmVar l k x | l < length ctx -> snd $ ctx !! l
+    TmVar l k x | l >= 0 && l < length ctx -> snd $ ctx !! l
     TmApp t1 t2 ->
       case synth ctx t1 of
         TyArrow ty1 ty2 | check ctx t2 ty1 -> ty2
