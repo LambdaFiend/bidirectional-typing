51cd90aa77d48c8f0cbf093905ecba56a3298d54
Gabriel Carlos <42923229+LambdaFiend@users.noreply.github.com>
2026-03-12 01:19:32 +0000

    Quick fix

--- src/Typing.hs
+++ src/Typing.hs
@@ -13,8 +13,9 @@ synth ctx t = let tm = getTm t in
     TmApp t1 t2 ->
       case synth ctx t1 of
         TyArrow ty1 ty2 | check ctx t2 ty1 -> ty2
+        _ -> TyError $ removeOuterParens $ showTerm (map fst ctx) t
     TmAnno t1 ty1 | check ctx t1 ty1 -> ty1
-    _ -> TyError $ showTerm (map fst ctx) t
+    _ -> TyError $ removeOuterParens $ showTerm (map fst ctx) t
 
 check :: TypeContext -> TermNode -> Type -> Bool
 check ctx t ty = let tm = getTm t in
